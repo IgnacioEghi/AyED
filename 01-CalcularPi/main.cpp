2daccfb69850528f232c7322092492991d13c17e
IgnacioEghi <135254468+IgnacioEghi@users.noreply.github.com>
2023-06-01 13:28:03 -0300

    Add files via upload

---
+++ 01-CalcularPi/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+#include <conio.h>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    float pi_deseado=3.141592;
+    float pi_sobre_cuatro=0;
+    float pi=0;
+    int iteraciones=0;
+    while (pi!=pi_deseado) {
+        pi_sobre_cuatro=pi_sobre_cuatro+pow(-1,iteraciones)/(2*iteraciones+1);
+        iteraciones++;
+        pi=pi_sobre_cuatro*4;
+    }
+    cout << "Iteraciones: " << iteraciones << endl;
+    cout << fixed;
+    cout << setprecision(6) << "Pi = " << pi << endl;
+    getch();
+    return 0;
+}
+
